7f16cfd49dc7317b2141247f6438bc3e32275f3a
bomafiber <bomafiber@yahoo.co.uk>
2019-05-20 20:09:39 +0100

    updated CSS and added code to start the game

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// declare variables
+
+let userSeq = [];
+let compSeq = [];
+let strictMode = false;
+let power = false;
+let sound = true;
+let winTheGame;
+let userTurn;
+let intervalId;
+let buttonFlash;
+let goodPlay
+let compTurn;
+
+
+const countTurn = document.querySelector("#playerturn");
+const powerOn = document.querySelector("#power");
+const startButton = document.querySelector("#start");
+const strictOn = document.querySelector("#strict")
+const greenquadrant = document.querySelector("#greenquad");
+const redquadrant = document.querySelector("#redquad");
+const bluequadrant = document.querySelector("#bluequad");
+const yellowquadrant = document.querySelector("#yellowquad");
+
+// start the GAME
+
+powerOn.addEventListener('check', (event) => {
+  if (powerOn.checked == true) {
+    powerOn = true
+  }else {
+    powerOn = false;
+  }
+});
