5df5975bb687d49cbc048235a9e02a2277406094
bomafiber <bomafiber@yahoo.co.uk>
2019-06-27 16:03:41 +0100

    Updted indexTest.html and created backup for game.js file

--- testing/spec/gameSpec.js
+++ testing/spec/gameSpec.js
@@ -1,15 +1,22 @@
+// describe('defined value', function() {
+//         it("should return a defined value", function() {
+//           let attributes = getAttributes();
+//           expect(attributes).toBeDefined();
+//         });
+//
+//
+//   it("should return attributes for green button when passed 1 as argument", function(){
+//     let greenAttributes = getAttributes(1);
+//     expect(greenAttributes).toBe([audio1, greenquadrant, "greenlite"])
+//   })
+//
+// })
 
 
-describe('defined value', function() {
-        it("should return a defined value", function() {
-          let attributes = getAttributes();
-          expect(attributes).toBeDefined();
-        });
-
-
-  it("should return attributes for green button when passed 1 as argument", function(){
-    let greenAttributes = getAttributes(1);
-    expect(greenAttributes).toBe([audio1, greenquadrant, "greenlite"])
-  })
-
+describe('description', function() {
+  it("generates random number between 1 and 4", function(){
+  var random = randomNumber();
+  expect(random).isNotGreaterThan(4)
 })
+
+});
