54966507bf212f56396069d1a3831f5e3d80de45
bomafiber <bomafiber@yahoo.co.uk>
2019-07-01 13:42:08 +0100

    Added specs test

--- game.spec.js
+++ game.spec.js
@@ -1,3 +1,15 @@
-describe('This is seen in the spec runner',function(){
+describe('getAttributes should return a defined value',function(){
+ it("should return a defined value",function(){
+  
+ });
+});
 
-})
+
+
+describe("randon Number should with limits",function(){
+  it("should return a random number between 1 and 4",function(){
+    let random = randomNumber();
+    expect(random).not.toBeGreaterThan(4)
+    expect(random).not.LessThan(1)
+  });
+});
